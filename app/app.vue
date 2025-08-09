7381acf1128bec9b83a529bb97f8fac525013784
Mehdi Absalan <116950499+mehdi-absln@users.noreply.github.com>
2025-08-10 03:18:54 +0330

    first commit

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+   hello world
+    <div class="text-3xl font-bold underline">
+      Hello world!
+    </div>
+    <div class="text-2xl font-semibold">
+      Welcome to your Vue.js application.
+    </div>
+    <div class="text-lg">
+      This is a simple example of a Vue.js application using Tailwind CSS for styling.
+  </div>
+  </div>
+</template>
