ffe1e1141f6fbe6f073c0fce4be631edfc34342d
Mehdi Absalan <116950499+mehdi-absln@users.noreply.github.com>
2025-12-07 15:48:37 +0330

    refactor: remove redundant types-components.ts file

--- app/components/courses/CoursesGrid.vue
+++ app/components/courses/CoursesGrid.vue
@@ -46,7 +46,14 @@
 
 <script setup lang="ts">
 import CourseCard from '~/components/courses/CourseCard.vue'
-import type { CoursesGridProps } from '~/types/components/types-components'
+
+interface CoursesGridProps {
+  courses: import('~/types/shared/courses').Course[]
+  loading: boolean
+  currentPage: number
+  totalPages: number
+  onPageChange: (page: number) => void
+}
 
 defineProps<CoursesGridProps>()
 </script>
