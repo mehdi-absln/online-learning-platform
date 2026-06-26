36516102175bc52af70ff5afc27fe42d3678dc07
unknown <mehdiabsalan7@gmail.com>
2026-06-27 02:10:48 +0330

    chore: remove unused scripts directory

--- scripts/check-reading-time.ts
+++
@@ -1,9 +0,0 @@
-// scripts/check-reading-time.ts
-import Database from 'better-sqlite3'
-import path from 'path'
-
-const dbPath = path.resolve(process.cwd(), 'server/data/db.sqlite')
-const db = new Database(dbPath)
-
-const result = db.prepare('SELECT id, title, reading_time FROM blogs').all()
-db.close()
